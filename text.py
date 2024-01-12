d60d666a7374d647a329e9d4ae0f958c913a2326
Alex Wu <108106616+Alexwu0706@users.noreply.github.com>
2024-01-11 16:28:55 -0800

    text python code

---
+++ text.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = "EC_180_DA_DB"
+    if x == "EC_180_DA_DB":
+        print("You are living in 2017")
+    else:
+        x = x+ " - Best class ever"
+        print(x)
