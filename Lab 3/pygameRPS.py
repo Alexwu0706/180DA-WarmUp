8f78ebf870307bb660d473be038b3a05962fbb76
Alex Wu <108106616+Alexwu0706@users.noreply.github.com>
2024-01-25 22:36:34 -0800

    Update

---
+++ Lab 3/pygameRPS.py
@@ -0,0 +1,29 @@
+# Import the pygame module
+import pygame
+# Import random for random numbers
+import random
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class rock(pygame.sprite.Sprite):
+
+
+class paper(pygame.sprite.Sprite):
+
+
+class scissor(pygame.sprite.Sprite):
