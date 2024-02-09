408718a59b64986404965cc7c242dbafbdd342e4
Alex Wu <108106616+Alexwu0706@users.noreply.github.com>
2024-02-08 18:37:16 -0800

    update

--- Lab 4/MQTT/MQTT.ino
+++ Lab 4/MQTT/MQTT.ino
@@ -1,59 +1,50 @@
- #include <WiFi.h>
- #include <PubSubClient.h>
- #include "arduino_secrets.h"
- #include "ICM_20948.h" // Click here to get the library:http://librarymanager/All#SparkFun_ICM_20948_IMU
- //#define USE_SPI // Uncomment this to use SPI
- #define SERIAL_PORT Serial
- #define SPI_PORT SPI // Your desired SPI port. Used only when "USE_SPI" is defined
- #define CS_PIN 2 // Which pin you connect CS to. Used only when "USE_SPI" is defined
- #define WIRE_PORT Wire // Your desired Wire port. Used when "USE_SPI" is not defined
- // The value of the last bit of the I2C address.
- // On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
- #define AD0_VAL 1
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "arduino_secrets.h"
+#include "ICM_20948.h" // Click here to get the library:http://librarymanager/All#SparkFun_ICM_20948_IMU
+//#define USE_SPI // Uncomment this to use SPI
+#define SERIAL_PORT Serial
+#define SPI_PORT SPI // Your desired SPI port. Used only when "USE_SPI" is defined
+#define CS_PIN 2 // Which pin you connect CS to. Used only when "USE_SPI" is defined
+#define WIRE_PORT Wire // Your desired Wire port. Used when "USE_SPI" is not defined
+// The value of the last bit of the I2C address.
+// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
+#define AD0_VAL 1
 
- #ifdef USE_SPI
- ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
- #else
- ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
- #endif
+#ifdef USE_SPI
+ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
+#else
+ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
+#endif
 
- // WiFi
- const char *ssid = SECRET_SSID; // Enter your WiFi name
- const char *password = SECRET_PASS; // Enter WiFi password
+// WiFi
+const char *ssid = SECRET_SSID; // Enter your WiFi name
+const char *password = SECRET_PASS; // Enter WiFi password
 
- // MQTT Broker
- // const char *mqtt_broker = "broker.emqx.io";
- // const char *topic = "ece180d/test";
- // const char *mqtt_username = "emqx";
- // const char *mqtt_password = "public";
- // const int mqtt_port = 1883;
+// MQTT Broker
+const char *mqtt_broker = "mqtt.eclipseprojects.io";
+const char *topic = "lol123";
+const int mqtt_port = 1883;
 
- // MQTT Broker
- const char *mqtt_broker = "mqtt.eclipseprojects.io";
+WiFiClient espClient;
+PubSubClient client(espClient);
 
- const char *topic = "lol123";
- // const char *mqtt_username = "emqx";
- // const char *mqtt_password = "public";
- const int mqtt_port = 1883;
-
- WiFiClient espClient;
- PubSubClient client(espClient);
-
- void setup() {
-   // Set software serial baud to 115200;
-   Serial.begin(115200);
-   // connecting to a WiFi network
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.println("Connecting to WiFi..");
-   }
-   Serial.println("Connected to the WiFi network");
-   //connecting to a mqtt broker
-   client.setServer(mqtt_broker, mqtt_port);
-   client.setCallback(callback);
-   
-   while (!client.connected()) {
+void setup() {
+ // Set software serial baud to 115200;
+ Serial.begin(115200);
+ printScaledAGMT(&myICM);
+ // connecting to a WiFi network
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+ }
+ Serial.println("Connected to the WiFi network");
+ //connecting to a mqtt broker
+ client.setServer(mqtt_broker, mqtt_port);
+ client.setCallback(callback);
+ 
+ while (!client.connected()) {
    String client_id = "esp32-client-";
    client_id += String(WiFi.macAddress());
    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
@@ -64,37 +55,38 @@
      Serial.print(client.state());
      delay(2000);
    }
-   }
-   // publish and subscribe
-   client.publish(topic, "Hi I’m ESP32 ^^");
-   client.subscribe(topic);
-  
+ }
+
+ // publish and subscribe
+ client.publish(topic, "Hi I’m ESP32 ^^");
+ client.subscribe(topic);
+
+ #ifdef USE_SPI
+ SPI_PORT.begin();
+ #else
+ WIRE_PORT.begin();
+ WIRE_PORT.setClock(400000);
+ #endif
+
+ bool initialized = false;
+ while (!initialized) {
    #ifdef USE_SPI
-   SPI_PORT.begin();
+   myICM.begin(CS_PIN, SPI_PORT);
    #else
-   WIRE_PORT.begin();
-   WIRE_PORT.setClock(400000);
+   myICM.begin(WIRE_PORT, AD0_VAL);
    #endif
   
-   bool initialized = false;
-   while (!initialized) {
-     #ifdef USE_SPI
-     myICM.begin(CS_PIN, SPI_PORT);
-     #else
-     myICM.begin(WIRE_PORT, AD0_VAL);
-     #endif
-    
-     SERIAL_PORT.print(F("Initialization of the sensor returned: "));
-     SERIAL_PORT.println(myICM.statusString());
-     if (myICM.status != ICM_20948_Stat_Ok) {
-       SERIAL_PORT.println("Trying again...");
-       delay(500);
-     } else {
-       initialized = true;
-    
-     }
+   SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+   SERIAL_PORT.println(myICM.statusString());
+   if (myICM.status != ICM_20948_Stat_Ok) {
+     SERIAL_PORT.println("Trying again...");
+     delay(500);
+   } else {
+     initialized = true;
+  
    }
  }
+}
 
 void callback(char *topic, byte *payload, unsigned int length) {
    Serial.print("Message arrived in topic: ");
@@ -109,14 +101,21 @@ void callback(char *topic, byte *payload, unsigned int length) {
 
 void loop()
 {
-
+   int predictedidleP = 0;
+   int predictedidleN = 0;
    if (myICM.dataReady())
    {
      myICM.getAGMT(); // The values are only updated when you call ’getAGMT’
-     // printRawAGMT( myICM.agmt ); // Uncomment this to see the raw values, taken directly from the agmt structure printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
-     // client.publish(topic, myICM->acc.axes.x);
+     printRawAGMT( myICM.agmt ); // Uncomment this to see the raw values, taken directly from the agmt structure printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+     //client.publish(topic, myICM->acc.axes.x);
      client.loop();
-     delay(30);
+     delay(300);
+
+     if(predicted_idle(myICM.agmt)){
+        predictedidleP += 1;
+      }else{
+        predictedidleN += 1;
+     }
    }
    else
    {
@@ -124,157 +123,188 @@ void loop()
      client.loop();
      delay(500);
    }
+
+   SERIAL_PORT.print("predicted idle: " +  String(predictedidleP) + " and predicted non-idle: " + String(predictedidleN));
 }
 
  // Below here are some helper functions to print the data nicely!
 
- void printPaddedInt16b(int16_t val)
+void printPaddedInt16b(int16_t val)
+{
+ if (val > 0)
  {
-   if (val > 0)
+   SERIAL_PORT.print(" ");
+   if (val < 10000)
    {
-     SERIAL_PORT.print(" ");
-     if (val < 10000)
-     {
-        SERIAL_PORT.print("0");
-     }
-     if (val < 1000)
-     {
-        SERIAL_PORT.print("0");
-     }
-     if (val < 100)
-     {
-        SERIAL_PORT.print("0");
-     }
-     if (val < 10)
-     {
-        SERIAL_PORT.print("0");
-    
-     }
+      SERIAL_PORT.print("0");
    }
-   else
+   if (val < 1000)
    {
-     SERIAL_PORT.print("-");
-     if (abs(val) < 10000)
-     {
-        SERIAL_PORT.print("0");
-     }
-     if (abs(val) < 1000)
-     {
-        SERIAL_PORT.print("0");
-     }
-     if (abs(val) < 100)
-     {
-        SERIAL_PORT.print("0");
-     }
-     if (abs(val) < 10)
-     {
-        SERIAL_PORT.print("0");
-     }
+      SERIAL_PORT.print("0");
    }
-   SERIAL_PORT.print(abs(val));
-}
-
- void printRawAGMT(ICM_20948_AGMT_t agmt)
- {
-   SERIAL_PORT.print("RAW. Acc [ ");
-   printPaddedInt16b(agmt.acc.axes.x);
-   SERIAL_PORT.print(", ");
-   printPaddedInt16b(agmt.acc.axes.y);
-   SERIAL_PORT.print(", ");
-   printPaddedInt16b(agmt.acc.axes.z);
-   SERIAL_PORT.print(" ], Gyr [ ");
-   printPaddedInt16b(agmt.gyr.axes.x);
-   SERIAL_PORT.print(", ");
-   printPaddedInt16b(agmt.gyr.axes.y);
-   SERIAL_PORT.print(", ");
-   printPaddedInt16b(agmt.gyr.axes.z);
-   SERIAL_PORT.print(" ], Mag [ ");
-   printPaddedInt16b(agmt.mag.axes.x);
-   SERIAL_PORT.print(", ");
-   printPaddedInt16b(agmt.mag.axes.y);
-   SERIAL_PORT.print(", ");
-   printPaddedInt16b(agmt.mag.axes.z);
-   SERIAL_PORT.print(" ], Tmp [ ");
-   printPaddedInt16b(agmt.tmp.val);
-   SERIAL_PORT.print(" ]");
-   SERIAL_PORT.println();
-}
-
- void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
- {
-   float aval = abs(val);
-   if (val < 0)
+   if (val < 100)
    {
-      SERIAL_PORT.print("-");
+      SERIAL_PORT.print("0");
    }
-   else
+   if (val < 10)
    {
-      SERIAL_PORT.print(" ");
-   }
-   
-   for (uint8_t indi = 0; indi < leading; indi++)
-   {
-     uint32_t tenpow = 0;
-     if (indi < (leading - 1))
-     {
-        tenpow = 1;
-     }
-     
-     for (uint8_t c = 0; c < (leading - 1 - indi); c++)
-     {
-        tenpow *= 10;
-     }
-     
-     if (aval < tenpow)
-     {
-        SERIAL_PORT.print("0");
-     }
-     else
-     {
-        break;
-     }
-   }
-   if (val < 0)
-   {
-   SERIAL_PORT.print(-val, decimals);
-   }
-   else
-   {
-   SERIAL_PORT.print(val, decimals);
+      SERIAL_PORT.print("0");
+  
    }
  }
+ else
+ {
+   SERIAL_PORT.print("-");
+   if (abs(val) < 10000)
+   {
+      SERIAL_PORT.print("0");
+   }
+   if (abs(val) < 1000)
+   {
+      SERIAL_PORT.print("0");
+   }
+   if (abs(val) < 100)
+   {
+      SERIAL_PORT.print("0");
+   }
+   if (abs(val) < 10)
+   {
+      SERIAL_PORT.print("0");
+   }
+ }
+ SERIAL_PORT.print(abs(val));
+}
 
- #ifdef USE_SPI
- void printScaledAGMT(ICM_20948_SPI *sensor)
- {
- #else
- void printScaledAGMT(ICM_20948_I2C *sensor)
- {
-   #endif
-   SERIAL_PORT.print("Scaled. Acc (mg) [ ");
-   printFormattedFloat(sensor->accX(), 5, 2);
-   char buf[10];
-   snprintf(buf, 10, "%f", sensor->accX());
-   client.publish(topic, buf);
-   SERIAL_PORT.print(", ");
-   printFormattedFloat(sensor->accY(), 5, 2);
-   SERIAL_PORT.print(", ");
-   printFormattedFloat(sensor->accZ(), 5, 2);
-   SERIAL_PORT.print(" ], Gyr (DPS) [ ");
-   printFormattedFloat(sensor->gyrX(), 5, 2);
-   SERIAL_PORT.print(", ");
-   printFormattedFloat(sensor->gyrY(), 5, 2);
-   SERIAL_PORT.print(", ");
-   printFormattedFloat(sensor->gyrZ(), 5, 2);
-   SERIAL_PORT.print(" ], Mag (uT) [ ");
-   printFormattedFloat(sensor->magX(), 5, 2);
-   SERIAL_PORT.print(", ");
-   printFormattedFloat(sensor->magY(), 5, 2);
+void printRawAGMT(ICM_20948_AGMT_t agmt)
+{
+ SERIAL_PORT.print("RAW. Acc [ ");
+ printPaddedInt16b(agmt.acc.axes.x);
+ SERIAL_PORT.print(", ");
+ printPaddedInt16b(agmt.acc.axes.y);
+ SERIAL_PORT.print(", ");
+ printPaddedInt16b(agmt.acc.axes.z);
+ SERIAL_PORT.print(" ], Gyr [ ");
+ printPaddedInt16b(agmt.gyr.axes.x);
+ SERIAL_PORT.print(", ");
+ printPaddedInt16b(agmt.gyr.axes.y);
+ SERIAL_PORT.print(", ");
+ printPaddedInt16b(agmt.gyr.axes.z);
+ SERIAL_PORT.print(" ], Mag [ ");
+ printPaddedInt16b(agmt.mag.axes.x);
+ SERIAL_PORT.print(", ");
+ printPaddedInt16b(agmt.mag.axes.y);
+ SERIAL_PORT.print(", ");
+ printPaddedInt16b(agmt.mag.axes.z);
+ SERIAL_PORT.print(" ], Tmp [ ");
+ printPaddedInt16b(agmt.tmp.val);
+ SERIAL_PORT.print(" ]");
+ SERIAL_PORT.println();
+}
+
+bool predicted_idle(ICM_20948_AGMT_t agmt){
+  bool predicted = true;
+  if((16000<agmt.acc.axes.z<17000) && (-350>agmt.acc.axes.y>-650) && (-800>agmt.acc.axes.x>-1200)){
+    predicted = true;
+  }else{
+    predicted = false;
+  }
+}
+
+
+int classifier(ICM_20948_AGMT_t agmt)
+{
+   int forwardcount = 0;
+   int upwardcount = 0;
+   int noactioncount = 0;
   
-   SERIAL_PORT.print(", ");
-   printFormattedFloat(sensor->magZ(), 5, 2);
-   SERIAL_PORT.print(" ], Tmp (C) [ ");
-   printFormattedFloat(sensor->temp(), 5, 2);
-   SERIAL_PORT.print(" ]");
-   SERIAL_PORT.println();
+  if(agmt.acc.axes.x){
+    forwardcount += 1;
+  }
+
+  if(agmt.acc.axes.z){
+    upwardcount += 1;
+  }
+
+  if(predicted_idle(myICM.agmt) == true){
+    noactioncount += 1;
+  }
+}
+
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+{
+ float aval = abs(val);
+ if (val < 0)
+ {
+    SERIAL_PORT.print("-");
+ }
+ else
+ {
+    SERIAL_PORT.print(" ");
+ }
+ 
+ for (uint8_t indi = 0; indi < leading; indi++)
+ {
+   uint32_t tenpow = 0;
+   if (indi < (leading - 1))
+   {
+      tenpow = 1;
+   }
+   
+   for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+   {
+      tenpow *= 10;
+   }
+   
+   if (aval < tenpow)
+   {
+      SERIAL_PORT.print("0");
+   }
+   else
+   {
+      break;
+   }
+ }
+ if (val < 0)
+ {
+ SERIAL_PORT.print(-val, decimals);
+ }
+ else
+ {
+ SERIAL_PORT.print(val, decimals);
+ }
+}
+
+#ifdef USE_SPI
+void printScaledAGMT(ICM_20948_SPI *sensor)
+{
+#else
+void printScaledAGMT(ICM_20948_I2C *sensor)
+{
+ #endif
+ SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+ printFormattedFloat(sensor->accX(), 5, 2);
+ char buf[10];
+ snprintf(buf, 10, "%f", sensor->accX());
+ client.publish(topic, buf);
+ SERIAL_PORT.print(", ");
+ printFormattedFloat(sensor->accY(), 5, 2);
+ SERIAL_PORT.print(", ");
+ printFormattedFloat(sensor->accZ(), 5, 2);
+ SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+ printFormattedFloat(sensor->gyrX(), 5, 2);
+ SERIAL_PORT.print(", ");
+ printFormattedFloat(sensor->gyrY(), 5, 2);
+ SERIAL_PORT.print(", ");
+ printFormattedFloat(sensor->gyrZ(), 5, 2);
+ SERIAL_PORT.print(" ], Mag (uT) [ ");
+ printFormattedFloat(sensor->magX(), 5, 2);
+ SERIAL_PORT.print(", ");
+ printFormattedFloat(sensor->magY(), 5, 2);
+
+ SERIAL_PORT.print(", ");
+ printFormattedFloat(sensor->magZ(), 5, 2);
+ SERIAL_PORT.print(" ], Tmp (C) [ ");
+ printFormattedFloat(sensor->temp(), 5, 2);
+ SERIAL_PORT.print(" ]");
+ SERIAL_PORT.println();
 }
---
+++ Lab 4/MQTT/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "abc"
+#define SECRET_PASS "Oddrat94"
